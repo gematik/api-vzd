9b0b0e5ff73f97fddf46c43f0dd4ada91df24170
spilikin <108782+spilikin@users.noreply.github.com>
2022-07-08 09:47:46 +0200

    Add ValueSet for `HealthcareService.specialty`

--- src/fhir/epa-valuesets.py
+++ src/fhir/epa-valuesets.py
@@ -27,12 +27,10 @@ def generate_value_set(url, output_file, template):
 
 generate_value_set(
   url=f"https://raw.githubusercontent.com/gematik/api-ePA/{VERSION}/src/vocabulary/value_sets/vs-practice-setting-code.xml",
-  output_file="input/fsh/valuesets/OrganizationTypeVS.fsh",
-  template="""ValueSet: OrganizationTypeVS
-Description: "ValueSet for `Organization.type`"
-* insert Meta
-* include codes from system OrganizationProfessionOID
-* include codes from system OrganizationProviderType"""
+  output_file="input/fsh/valuesets/HealthcareServiceSpecialtyVS.fsh",
+  template="""ValueSet: HealthcareServiceSpecialtyVS
+Description: "ValueSet for `HealthcareService.specialty`"
+* insert Meta"""
 )
 
 generate_value_set(
