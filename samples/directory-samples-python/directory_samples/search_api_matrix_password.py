f0966f1db70c7d159ecf5d3805300c477bd941a6
spilikin <108782+spilikin@users.noreply.github.com>
2023-03-17 10:39:46 +0100

    Update samples output

--- samples/directory-samples-python/directory_samples/search_api_matrix_password.py
+++ samples/directory-samples-python/directory_samples/search_api_matrix_password.py
@@ -1,30 +1,33 @@
 from urllib.parse import urljoin
 from requests import (Session)
-from rich import (get_console)
 from .apitools import (request_to_curl, response_to_text, jwt_to_text)
 from .config import (MatrixPasswordConfig, FHIRDirectoryConfig)
+from rich.console import Console
+
+debug_console = Console(record=True, stderr=True)
+debug_print = debug_console.print
 
 
 def main():
     matrix_config = MatrixPasswordConfig()
 
-    print = get_console().print
+    debug_print("[b]Login to Directory using Matrix password authentication[b]")
 
-    print("Matrix configuration:")
-    print(matrix_config)
+    debug_print("\n[b]Matrix configuration:[b]")
+    debug_print(matrix_config)
 
-    print("FHIR-Directory configuration:")
+    debug_print("\n[b]FHIR-Directory configuration:[/b]")
     fhir_directory_config = FHIRDirectoryConfig()
-    print(fhir_directory_config)
+    debug_print(fhir_directory_config)
 
     session = Session()
 
-    print("Get  authentication methods supported by the Matrix homeserver")
-    # s. https://matrix.org/docs/guides/client-server-api#login
+    debug_print("\n[b]Get authentication methods supported by the Matrix homeserver[/b]")
+    debug_print("  See https://matrix.org/docs/guides/client-server-api#login")
     response = session.get(urljoin(matrix_config.homeserver_url, "/_matrix/client/r0/login"))
 
-    print(request_to_curl(response.request))
-    print(response_to_text(response))
+    debug_print(request_to_curl(response.request))
+    debug_print(response_to_text(response))
 
     response = session.post(
       urljoin(matrix_config.homeserver_url, "/_matrix/client/r0/login"),
@@ -36,9 +39,9 @@ def main():
 
     )
 
-    print("Perform login and get Matrix Access Token:")
-    print(request_to_curl(response.request))
-    print(response_to_text(response))
+    debug_print("\n[b]Perform login and get Matrix Access Token:[b]")
+    debug_print(request_to_curl(response.request))
+    debug_print(response_to_text(response))
 
     matrix_access_token = response.json()["access_token"]
 
@@ -51,9 +54,9 @@ def main():
       }
     )
 
-    print("Request Matrix OpenID Token:")
-    print(request_to_curl(response.request))
-    print(response_to_text(response))
+    debug_print("\n[b]Request Matrix OpenID Token:[/b]")
+    debug_print(request_to_curl(response.request))
+    debug_print(response_to_text(response))
 
     matrix_opendid_token = response.json()["access_token"]
 
@@ -65,19 +68,20 @@ def main():
       },
     )
 
-    print("FHIR-Directory /tim-authenticate response")
-    print(request_to_curl(response.request), soft_wrap=True)
-    print(response_to_text(response))
+    debug_print("\n[b]FHIR-Directory /tim-authenticate response[/b]")
+    debug_print(request_to_curl(response.request), soft_wrap=True)
+    debug_print(response_to_text(response))
 
     assert response.status_code == 200
-    
+
     search_api_access_token = response.json()["access_token"]
 
-    print("/search API access_token:")
-    print(jwt_to_text(search_api_access_token))
-    print(search_api_access_token, soft_wrap=True)
+    debug_print("\n[b]/search API access_token:[/b]")
+    debug_print(search_api_access_token, soft_wrap=True)
+    debug_print("\n[b]/search API access_token details[/b]")
+    debug_print(jwt_to_text(search_api_access_token))
 
-    print("Perform FHIR-Search")
+    debug_print("\n[b]Perform FHIR-Search[/b]")
     response = session.get(
         urljoin(fhir_directory_config.search_url, "HealthcareService"),
         params={
@@ -89,5 +93,7 @@ def main():
         }
     )
 
-    print(request_to_curl(response.request), soft_wrap=True)
-    print(response_to_text(response))
+    debug_print(request_to_curl(response.request), soft_wrap=True)
+    debug_print(response_to_text(response))
+
+    print(search_api_access_token)
